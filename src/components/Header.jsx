ac669c93322fe5fcc8b096d69d7dde13cf9cdeb3
vatih2675 <ruslansamuel11@gmail.com>
2025-11-24 14:29:22 +0800

    relink title header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ import logoMerah from "/logo-merah.png";
 export default function Header({ dataDesa, halaman, changeHalaman }) {
   return (
     <header className="w-full bg-white/75 sticky top-0 left-0 right-0 px-20 py-3 flex justify-between items-center cursor-default z-99 shadow-black/10 shadow-lg">
-      <div className="flex justify-center items-center gap-2">
+      <a
+        href="/"
+        className="flex justify-center items-center gap-2 cursor-pointer"
+      >
         <img src={logoMerah} alt="Logo" className="w-10" />
         <div className="flex flex-col justify-center items-start">
           <h1 className="font-bold uppercase text-lg">
@@ -15,7 +18,7 @@ export default function Header({ dataDesa, halaman, changeHalaman }) {
             Kec. {dataDesa.kecamatan}, {dataDesa.kota_kab}
           </h3>
         </div>
-      </div>
+      </a>
       <div className="flex justify-center items-center gap-10 text-lg">
         <NavLink
           to="/"
